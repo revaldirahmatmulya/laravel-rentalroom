a13cde8546fd8ec934be5360b13a23156ec2ad3d
revaldirahmatmulya <revaldiaye@gmail.com>
2023-05-30 00:36:02 +0700

    fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,20 +7,16 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // \App\Models\User::factory(10)->create();
-<<<<<<< HEAD
-        $this->call(RuangSeeder::class);
-        $this->call(MahasiswaSeeder::class);
-        $this->call(AdminSeeder::class);
-=======
-        $this->call(MahasiswaSeeder::class);
->>>>>>> 8616a0082ddbafbdcf980a5885fa34ad4acae981
-    }
+        /**
+         * Seed the application's database.
+         *
+         * @return void
+         */
+        public function run()
+        {
+                // \App\Models\User::factory(10)->create();
+                $this->call(RuangSeeder::class);
+                $this->call(MahasiswaSeeder::class);
+                $this->call(AdminSeeder::class);
+        }
 }

1b223da703b2b85beedb7cecbf67aeecf5a41086
revaldirahmatmulya <revaldiaye@gmail.com>
2023-05-30 00:35:37 +0700

    fix

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -4,12 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-<<<<<<< HEAD
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
-=======
->>>>>>> 8616a0082ddbafbdcf980a5885fa34ad4acae981
 
 class AdminSeeder extends Seeder
 {
@@ -20,7 +17,6 @@ class AdminSeeder extends Seeder
      */
     public function run()
     {
-<<<<<<< HEAD
         DB::table('admins')->delete();
         DB::table('admins')->insert([
             'username' => "admin",
@@ -28,8 +24,3 @@ class AdminSeeder extends Seeder
         ]);
     }
 }
-=======
-        //
-    }
-}
->>>>>>> 8616a0082ddbafbdcf980a5885fa34ad4acae981
